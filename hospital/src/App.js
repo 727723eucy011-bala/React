8067ad732fb4ebe625e5465080800d5e691e94c2
bala_mahendar <727723eucy011@skcet.ac.in>
2024-09-19 05:53:49 +0530

    19/09/2024

--- hospital/src/App.js
+++ hospital/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import Product from './components/exampler';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import MainPage from './components/MainPage';
-import { ThemeProvider } from './components/ThemeContext'; 
-import Homepage from './components/Homepage';
-import './App.css';
+import Navbar from './components/exampler';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+// import MainPage from './components/MainPage';
+// import { ThemeProvider } from './components/ThemeContext'; 
+// import Homepage from './components/Homepage';
+// import './Former.css';
 
 function App() {
   return (
-    // <Product />
-    <Router>
-      <ThemeProvider>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/mainpage" element={<MainPage />} />
-        <Route path="/Homepage" element={<Homepage />} />
-      </Routes>
-      </ThemeProvider>
-    </Router>
+    <Navbar />
+    // <Router>
+    //   <ThemeProvider>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/signup" element={<Signup />} />
+    //     <Route path="/mainpage" element={<MainPage />} />
+    //     <Route path="/Homepage" element={<Homepage />} />
+    //   </Routes>
+    //   </ThemeProvider>
+    // </Router>
   );
 }
 
